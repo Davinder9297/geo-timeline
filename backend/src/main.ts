3e8ef199f8f56651064990b83b6e10cab2eafef7
Davinder9297 <davindergiri765@gmail.com>
2026-06-18 20:25:48 +0530

    cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3002'],
+    origin: ['http://localhost:3001', 'http://localhost:3002','https://geo-timeline-trackers.vercel.app','https://geo-timeline-dashboard.vercel.app'],
     credentials: true,
   });
 
